31eb9a9d2127357a1f1fe0b98f7790e47f640428
nckbrrs <github@nickbarrs.com>
2023-03-30 00:17:16 -0400

    tweak styling of 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from 'next/router';
 import Head from "next/head";
+import { Col } from "../components/base";
+import 'twin.macro';
+
 
 const NotFound: React.FC = () => {
     const router = useRouter();
@@ -23,10 +26,10 @@ const NotFound: React.FC = () => {
         <Head>
             <title>Nick Barrs / 404</title>
         </Head>
-        <div>
-            <h1>Oops!</h1>
-            <p>In {secondsToWaitBeforeRerouting - secondsOnPage} seconds, you will be navigated to the <Link href="/">home page.</Link></p>
-        </div>
+        <Col tw="h-full justify-center items-center">
+            <p tw="w-full font-bold text-left">404 – There's nothing here!</p>
+            <p tw="w-full">In {secondsToWaitBeforeRerouting - secondsOnPage} seconds, you will be navigated to my <Link href="/">home page.</Link></p>
+        </Col>
         </>
     )
 }
