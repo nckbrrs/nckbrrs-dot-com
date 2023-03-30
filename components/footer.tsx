340b93754a2b6f3d4d3c48d842e1db17caa6cae5
nckbrrs <github@nickbarrs.com>
2023-03-30 00:32:13 -0400

    remove footer and add hamburger menu into logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,9 @@
-import { Col, Row } from './base';
-import ResumePdf from '../public/resume.pdf';
+import { Col } from './base';
 import 'twin.macro'
 
 const Footer: React.FC = () => {
     return (
-        <Col tw="px-3 pb-4 pt-6 sm:pb-10 md:pb-12 lg:pb-20">
-            <Row tw="justify-between items-center">
-                <a href={ResumePdf} target="_blank" rel="noopener noreferrer">
-                    <p tw="tracking-tightish font-mono font-normal lg:text-xl hover:font-semibold hover:drop-shadow-sm hover:-translate-y-[1px] duration-100 cursor-pointer">resume</p>
-                </a>
-                <a href="mailto:hello@nickbarrs.com">
-                    <p tw="tracking-tightish font-mono font-normal lg:text-xl hover:font-semibold hover:drop-shadow-sm hover:-translate-y-[1px] duration-100 cursor-pointer">contact</p>
-                </a>
-            </Row>
-        </Col>
+        <Col tw="px-3 pb-4 pt-6 sm:pb-10 md:pb-12 lg:pb-20"/>
     )
 }
 
--- components/header.tsx
+++ components/header.tsx
@@ -2,21 +2,200 @@ import Logo from './icons/Logo';
 import { Col, Row } from './base';
 import 'twin.macro';
 import Link from 'next/link';
+import { useState } from 'react';
+import tw, { styled } from 'twin.macro';
+import { AnimatePresence } from 'framer-motion';
+import ResumePdf from '../public/resume.pdf';
 
 const Header: React.FC = () => {
+    const [menuState, setMenuState] = useState<'open' | 'closed'>('closed');
+    const sessionStorage = (typeof window !== 'undefined') ? window.sessionStorage : null;
+
+    const toggleMenu = () => {
+        setMenuState(menuState === 'closed' ? 'open' : 'closed');
+    }
+
+    const onClickLogo = () => {
+        toggleMenu();
+        if (sessionStorage) {
+            sessionStorage.setItem('hasOpenedMenuAtLeastOnce', 'true')
+        }
+    }
+
+    const AnimatedLogo: JSX.Element = (
+        <LogoContainer
+            animate={{
+                rotate: ['0deg', '-8deg', '8deg', '-8deg', '8deg', '-8deg', '8deg', '0deg'],
+                scale: [1, 1.333, 1]
+            }}
+            transition={{
+                repeat: Infinity,
+                duration: 0.8,
+                ease: 'easeInOut',
+                delay: 1,
+                repeatDelay: 4
+            }}
+            onClick={onClickLogo}
+        >
+            <LogoWrapper>
+                <Logo/>
+            </LogoWrapper>
+        </LogoContainer>
+    );
+
+    const NonAnimatedLogo: JSX.Element = (
+        <LogoContainer onClick={onClickLogo}>
+            <LogoWrapper>
+                <Logo/>
+            </LogoWrapper>
+        </LogoContainer>
+    )
+
+    const hasOpenedMenuAtLeastOnce: boolean = Boolean(sessionStorage?.getItem('hasOpenedMenuAtLeastOnce') || false);
+
+    const links: {linkType: 'external' | 'local', text: string, href: string}[] = [
+        {
+            linkType: 'external',
+            text: 'apple music',
+            href: 'https://music.apple.com/us/artist/ephemera/1668905267'
+        },
+        {
+            linkType: 'external',
+            text: 'spotify',
+            href: 'https://open.spotify.com/artist/6P5suR8XUGZzqYyX5WpsCY'
+        },
+        {
+            linkType: 'external',
+            text: 'twitter',
+            href: 'https://www.twitter.com/nckbrrs'
+        },
+        {
+            linkType: 'external',
+            text: 'instagram',
+            href: 'https://www.instagram.com/nckbrrs'
+        },
+        {
+            linkType: 'external',
+            text: 'youtube',
+            href: 'https://www.youtube.com/@nckbrrs'
+        },
+        {
+            linkType: 'external',
+            text: 'github',
+            href: 'https://www.github.com/nckbrrs'
+        },
+        {
+            linkType: 'external',
+            text: 'linkedin',
+            href: 'https://www.linkedin.com/in/nckbrrs'
+        },
+        {
+            linkType: 'external',
+            text: 'resume',
+            href: ResumePdf
+        },
+        {
+            linkType: 'external',
+            text: 'email',
+            href: 'mailto:hello@nickbarrs.com'
+        },
+    ]
+
     return (
-        <Col tw="pt-5 sm:pt-10 md:pt-12 lg:pt-20">
-            <Row tw="justify-end items-center">
-                <Col tw="w-10 h-10 md:w-12 md:h-12 xl:w-16 xl:h-16 drop-shadow-sm">
-                    <Link href="/">
-                        <Logo/>
-                    </Link>
-                </Col>
-            </Row>
-        </Col>
+        <Container>
+            {hasOpenedMenuAtLeastOnce ? <>{NonAnimatedLogo}</> : <>{AnimatedLogo}</>}
+            <AnimatePresence>
+                {menuState === 'open' && 
+                    <MenuContainer 
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        <MenuLinksCol className="group">
+                            {links.map((l) => 
+                                l.linkType === 'external' ? (
+                                    <a tw="w-full" href={l.href} target="_blank" rel="noopener noreferrer">
+                                        <LinkText>{l.text.toUpperCase()}</LinkText>
+                                    </a>
+                                ) : (
+                                    <Link tw="w-full" href={`${l.href}`} >
+                                        <LinkText>{l.text.toUpperCase()}</LinkText>
+                                    </Link>
+                                )
+                            )}
+                        </MenuLinksCol>
+                    </MenuContainer>
+                }
+            </AnimatePresence>
+        </Container>
     )
 }
 
 export default Header;
 
+const MenuLinksCol = styled(Col)(() => [
+    tw`
+        flex-wrap
+        items-center
+        justify-center
+        space-y-1
+        translate-y-2
+    `
+])
 
+const Container = styled(Row)(() => [
+    tw`
+        justify-end
+        items-center
+        pt-6 sm:pt-10 md:pt-12 lg:pt-20
+    `
+])
+
+const LogoContainer = styled(Col)(() => [
+    tw`
+        z-20
+        w-12 xl:w-16
+        h-12 xl:h-16
+        drop-shadow-sm
+        cursor-pointer
+    `
+])
+
+const LogoWrapper = styled.span(() => [
+    tw`
+        duration-100
+        hover:scale-110
+    `
+])
+
+const LinkText = styled.p(() => [
+    tw`
+        w-full
+        px-2
+        text-3xl md:text-5xl lg:text-7xl
+        font-bold
+        italic 
+        text-left
+        duration-100
+        group-hover:opacity-25
+        group-hover:blur-[2px]
+        hover:!opacity-100
+        hover:!blur-0
+        hover:translate-x-3
+    `
+])
+
+const MenuContainer = styled(Row)(() => [
+    tw`
+        w-screen
+        h-screen
+        space-y-2
+        bg-bone
+        left-0
+        top-0
+        z-10
+        absolute
+        justify-center
+        items-center
+    `
+])
