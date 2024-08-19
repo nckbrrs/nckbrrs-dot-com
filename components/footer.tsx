4c135c697bf93c17f5aa2d61ddadfa09d433ebd4
nckbrrs <github@nickbarrs.com>
2024-08-19 18:45:33 -0400

    add "made with love" footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,9 @@ import 'twin.macro'
 
 const Footer: React.FC = () => {
     return (
-        <Col tw="px-3 pb-4 pt-6 sm:pb-10 md:pb-12 lg:pb-20"/>
+        <Col tw="pb-4 pt-6 sm:pb-10 md:pb-12 lg:pb-20 justify-end items-end">
+            <p tw="text-[0.75em] font-mono tracking-tightest">made with ❤️ by me</p>
+        </Col>
     )
 }
 
